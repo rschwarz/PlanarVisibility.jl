5462f0f1c9204c9e7f7c21009ce151ea21ca4777
Robert Schwarz <mail@rschwarz.net>
2019-04-02 15:15:21 +0200

    sketch some types

--- src/PlanarVisibility.jl
+++ src/PlanarVisibility.jl
@@ -1,3 +1,65 @@
 module PlanarVisibility
 
+using GeoInterface: Point, Polygon
+using LightGraphs: SimpleGraph
+
+#
+# Types
+#
+
+"Environment of obstacles."
+struct Environment
+    # TODO: make types parametric?
+    # TODO: support linestrings?
+    polygons::Array{Polygon}
+end
+
+"Visibility graph."
+struct VisibilityGraph
+    env::Environment               # initial environment
+    points::Array{Point}           # all points (including environment)
+    indices::Dict{Point, Int64}    # map from point to array index
+    graph::SimpleGraph{Int64}      # edges of visibility relation
+end
+
+VisibilityGraph(env::Environment) = construct_graph(env)
+
+
+#
+# Utilities
+#
+
+"Extract unique points from given environment."
+function extract_points(env::Environment)
+    points = Point[]
+    indices = Dict{Point, Int64}()
+
+    # for polygon in env.polygons
+
+    return points, indices
+end
+
+
+#
+# Constructing visibility graphs
+#
+
+function construct_graph(env::Environment)
+    # extract points from environment
+    points, indices = extract_points(env)
+
+    # build graph from disconnected points
+    n = length(points)
+    graph = SimpleGraph(n)
+
+    # assemble result
+    return VisibilityGraph(env, points, indices, graph)
+end
+
+
+#
+# Extending visibility graphs
+#
+
+
 end
