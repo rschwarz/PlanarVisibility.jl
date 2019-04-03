5a89858d2a88fc656553235347822a95d9b60987
Robert Schwarz <mail@rschwarz.net>
2019-04-03 23:08:37 +0200

    add more tests to extract_points
     - now works with GeoInterface#master

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,14 +12,39 @@ using PlanarVisibility: Environment, extract_points
 
     @testset "single polygon" begin
         env = Environment([
-            Polygon([[[0.0, 0.0], [1.0, 0.0], [0.0, 1.0]]])
+            Polygon([[[0.0, 0.0], [1.0, 0.0], [0.0, 1.0], [0.0, 0.0]]])
         ])
         points, indices = extract_points(env)
         @test length(points) == 3
         @test length(indices) == 3
         @test points[1] == Point([0.0, 0.0])
-        @test indices[points[1]] == 1
-        @test indices[points[2]] == 2
-        @test indices[points[3]] == 3
+        @test all(indices[points[i]] == i for i in 1:3)
+    end
+
+    @testset "polygon with hole" begin
+        env = Environment([
+            Polygon([[[0.0, 0.0], [1.0, 0.0], [0.0, 1.0], [0.0, 0.0]],
+                     [[0.1, 0.1], [0.1, 0.9], [0.9, 0.1], [0.1, 0.1]]])
+        ])
+        points, indices = extract_points(env)
+        @test length(points) == 6
+        @test length(indices) == 6
+        @test points[1] == Point([0.0, 0.0])
+        @test all(indices[points[i]] == i for i in 1:6)
+    end
+
+    @testset "two touching polygons" begin
+        env = Environment([
+            Polygon([
+                [[0.0, 0.0], [1.0, 0.0], [1.0, 1.0], [0.0, 1.0], [0.0, 0.0]],
+                [[1.0, 0.0], [2.0, 0.0], [2.0, 1.0], [1.0, 1.0], [1.0, 0.0]]
+            ])
+        ])
+        points, indices = extract_points(env)
+        @test length(points) == 6
+        @test length(indices) == 6
+        @test points[1] == Point([0.0, 0.0])
+        @test points[6] == Point([2.0, 1.0])
+        @test all(indices[points[i]] == i for i in 1:6)
     end
 end
